fc40aaf8fb605360f05c5139b0cc65d72dd9514e
fyucee <yudafyuce@gmail.com>
2015-12-28 13:58:35 +0700

    asem

--- application/views/beranda.php
+++ application/views/beranda.php
@@ -2,7 +2,7 @@
 	<div class="col-md-3">
     </div>                  
     <div class="col-md-6">
-    <img src="/assets/images/logo.png" style="width:100%;height:100%;">
+    <img src="<?php echo assets_url(); ?>/images/logorsmh.jpg" style="width:100%;height:100%;">
     </div>                  
 	<div class="col-md-3">
     </div>
